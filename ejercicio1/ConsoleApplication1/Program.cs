4cc23d0c14d43810ccad9960aa277e2c1e6d1a93
ivan1407 <ivan_d_quiroga@hotmail.com>
2021-04-07 15:25:14 -0300

    terminado

--- ejercicio1/ConsoleApplication1/Program.cs
+++ ejercicio1/ConsoleApplication1/Program.cs
@@ -10,6 +10,30 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Dictionary<string, string> values = new Dictionary<string, string>();
+            values.Add("Juan", "55423412");
+            values.Add("Ernesto", "56985623");
+            values.Add("Mariana", "54787451");
+            if (values.ContainsKey("Juan"))
+            {
+
+                Console.WriteLine(values["Juan"]);
+             
+            }
+            else { Console.WriteLine("no existe"); }
+            string pedro = "234235";
+            if (values.TryGetValue("pedro", out pedro)) { Console.WriteLine(values["pedro"]); }
+            else { Console.WriteLine("no contiene la llave"); }
+            foreach(KeyValuePair<string,string> value in values) { Console.WriteLine(value.Key + ", " + value.Value); }
+            values["Mariana"] = "58251425";
+            Console.WriteLine(values["nuevo telefono de Mariana"]);
+            Console.ReadLine();
+
         }
     }
+   
+  
 }
+
+
+
