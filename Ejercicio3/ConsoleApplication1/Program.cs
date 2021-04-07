da255a42c6c5518d31072429f5072eb51acdc50e
ivan1407 <ivan_d_quiroga@hotmail.com>
2021-04-07 17:15:16 -0300

    Update Program.cs

--- Ejercicio3/ConsoleApplication1/Program.cs
+++ Ejercicio3/ConsoleApplication1/Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("ingrese un parrafo\n");
+            string parrafo = Console.ReadLine();
+            string[] palabra = parrafo.Split(' ');
+            int cont=0;
+            foreach(string palabras in palabra) { cont++; }
+            for(int x = 0; x < palabra.Length; x++)
+            {
+                for (int i = 0; i < palabra.Length; i++) { if (palabra[i] == palabra[x]) cont++; }
+
+            }
+
         }
     }
 }
